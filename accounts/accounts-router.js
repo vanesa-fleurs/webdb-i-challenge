64e219a19943e79a5f06620782f81b1108afff67
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-04 14:33:57 -0600

    file structure and fist GET done

---
+++ accounts/accounts-router.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const db = require('../data/dbConfig.js')
+
+router.get('/', (req,res) => {
+    db.select('*').from('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(error => res.status(500).json({message: `error in retrieveing accounts`}))
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const server = express();
 
 server.use(express.json());
 
+const accountsRouter = require('./accounts/accounts-router.js')
+server.use('/api/accounts', accountsRouter)
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda WEP DB Challenge :)</h>
+      <p>Let's get started... </p>
+    `);
+});
+
 module.exports = server;
